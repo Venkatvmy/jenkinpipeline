6743e6fd05ae5cce8f09a97fc3478744f8831b82
Venkatvmy <venkat.veeray@gmail.com>
2025-08-26 03:25:17 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, Batch40 - create java pipeline !!!");
+        System.out.println("Hello, Jenkins Pipeline!");
     }
 }
